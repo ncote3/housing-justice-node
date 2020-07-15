f61f51c2d8e66d7a3506551b253a45ee0b913f2e
noahcote <noah.cote3@gmail.com>
2020-07-15 14:12:44 -0400

    made to work with s3 bucket

--- server.js
+++ server.js
@@ -1,18 +1,77 @@
+require('dotenv').config();
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
+const AWS = require('aws-sdk');
+const s3  = new AWS.S3({
+    accessKeyId: process.env.BUCKETEER_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.BUCKETEER_AWS_SECRET_ACCESS_KEY,
+    region: 'us-east-1',
+});
 
 const app = express();
 
 app.use(cors());
 
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (port == null || port === "") {
     port = 8000;
 }
 app.listen(port);
 
 console.log("listening at port: %s", port);
+
+const downloadJSONFromS3 = (filePath, bucketName, key) => {
+    const params = {
+        Bucket: bucketName,
+        Key: key
+    };
+    s3.getObject(params, (err, data) => {
+        if (err) console.error(err);
+        fs.writeFileSync(filePath, data.Body.toString());
+        console.log(`${filePath} has been created!`);
+    });
+}
+
+const filePath = './data/';
+const significant_sorted_landlords = 'significant_sorted_landlords.json';
+const landlords_and_properties = 'landlords_and_properties.json';
+const significant_landlords = 'significant_landlords.json';
+const landlords = 'landlords.json';
+
+if (!fs.existsSync(filePath+significant_sorted_landlords)) {
+    console.log('Trying to download significant_sorted_landlords data from AWS S3 bucket.');
+    downloadJSONFromS3(
+        filePath+significant_sorted_landlords,
+        process.env.BUCKETEER_BUCKET_NAME,
+        significant_sorted_landlords
+    );
+}
+if (!fs.existsSync(filePath+landlords_and_properties)) {
+    console.log('Trying to download landlords_and_properties data from AWS S3 bucket.');
+    downloadJSONFromS3(
+        filePath+landlords_and_properties,
+        process.env.BUCKETEER_BUCKET_NAME,
+        landlords_and_properties
+    );
+}
+if (!fs.existsSync(filePath+significant_landlords)) {
+    console.log('Trying to download significant_landlords data from AWS S3 bucket.');
+    downloadJSONFromS3(
+        filePath+significant_landlords,
+        process.env.BUCKETEER_BUCKET_NAME,
+        significant_landlords
+    );
+}
+if (!fs.existsSync(filePath+landlords)) {
+    console.log('Trying to download landlords data from AWS S3 bucket.');
+    downloadJSONFromS3(
+        filePath+landlords,
+        process.env.BUCKETEER_BUCKET_NAME,
+        landlords
+    );
+}
+
 
 app.get('/', (req, res) => {
     return res.send("Are ya coding son?");
