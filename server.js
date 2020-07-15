ada7c140352e52061a7759acd008c83cd5e2dda6
noahcote <noah.cote3@gmail.com>
2020-07-15 14:37:26 -0400

    updating

--- server.js
+++ server.js
@@ -80,36 +80,38 @@ app.get('/', (req, res) => {
 // Returns a object of landlords and how many properties they own.
 // Will be useful for populating a list of landlords to show on the map.
 app.get('/api/landlords', (req, res) => {
-    const landlords = JSON.parse(fs.readFileSync('./data/landlords.json', 'utf8'));
+    const landlords = JSON.parse(fs.readFileSync('./landlords.json', 'utf8'));
     return res.send(landlords);
 });
 
 // Returns a object of significant landlords (Own more than 50 Props) and how many properties they own.
 // Will be useful for testing.
 app.get('/api/significant-landlords', (req, res) => {
-    const landlords = JSON.parse(fs.readFileSync('./data/significant_landlords.json', 'utf8'));
+    const landlords = JSON.parse(fs.readFileSync('./significant_landlords.json', 'utf8'));
     return res.send({info: Object.keys(landlords)});
 });
 
 // Returns a object of significant landlords (Own more than 50 Props) and how many properties they own.
 // Will be useful for testing.
 app.get('/api/significant-sorted-landlords', (req, res) => {
-    const landlords = JSON.parse(fs.readFileSync('./data/significant_sorted_landlords.json', 'utf8'));
+    const landlords = JSON.parse(fs.readFileSync('./significant_sorted_landlords.json', 'utf8'));
     return res.send({info: landlords});
 });
 
+
+// Not used
 // Returns an object of addresses.
 // Can be used for auto-filling search boxes for landlord grouping.
-app.get('/api/properties', (req, res) => {
-    const properties = JSON.parse(fs.readFileSync('./data/properties.json', 'utf8'));
-    return res.send(JSON.stringify(properties));
-});
+// app.get('/api/properties', (req, res) => {
+//     const properties = JSON.parse(fs.readFileSync('./properties.json', 'utf8'));
+//     return res.send(JSON.stringify(properties));
+// });
 
 // Returns an object of landlords with data on addresses, geo coords, number of properties.
 // Useful for returning a smaller number of objects
 app.get('/api/landlords-and-properties/:landlord', (req, res) => {
     const landlordsAndProperties = JSON.parse(
-        fs.readFileSync('./data/landlords_and_properties.json', 'utf8')
+        fs.readFileSync('./landlords_and_properties.json', 'utf8')
     );
     return res.send(landlordsAndProperties[req.params.landlord]);
 });
