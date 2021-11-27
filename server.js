0dd573c06f1e29e01ce8b21c23adcede94684d45
Noah Cote <noah.cote@gmail.com>
2021-11-27 10:07:08 -0500

    linter & prettier

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env nodejs
-require('dotenv').config();
-const fs = require('fs');
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const fs = require("fs");
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,73 +10,82 @@ app.use(cors());
 
 let port = process.env.PORT;
 if (port == null || port === "") {
-    port = 8000;
+  port = 8000;
 }
 app.listen(port);
 
 console.log("listening at port: %s", port);
 
-app.get('/', (req, res) => {
-    return res.send("Are ya coding son?");
+app.get("/", (req, res) => {
+  return res.send("Are ya coding son?");
 });
 
 // Returns a object of landlords and how many properties they own.
 // Will be useful for populating a list of landlords to show on the map.
-app.get('/api/landlords', (req, res) => {
-    const landlords = JSON.parse(fs.readFileSync('./landlords.json', 'utf8'));
-    return res.send(landlords);
+app.get("/api/landlords", (req, res) => {
+  const landlords = JSON.parse(
+    fs.readFileSync("./data/landlords.json", "utf8")
+  );
+  return res.send(landlords);
 });
 
 // Returns a object of significant landlords (Own more than 50 Props) and how many properties they own.
 // Will be useful for testing.
-app.get('/api/significant-landlords', (req, res) => {
-    const landlords = JSON.parse(fs.readFileSync('./significant_landlords.json', 'utf8'));
-    return res.send({info: Object.keys(landlords)});
+app.get("/api/significant-landlords", (req, res) => {
+  const landlords = JSON.parse(
+    fs.readFileSync("./data/significant_landlords.json", "utf8")
+  );
+  return res.send({ info: Object.keys(landlords) });
 });
 
 // Returns a object of significant landlords (Own more than 50 Props) and how many properties they own.
 // Will be useful for testing.
-app.get('/api/significant-sorted-landlords', (req, res) => {
-    const landlords = JSON.parse(fs.readFileSync('./significant_sorted_landlords.json', 'utf8'));
-    return res.send({info: landlords});
+app.get("/api/significant-sorted-landlords", (req, res) => {
+  const landlords = JSON.parse(
+    fs.readFileSync("./data/significant_sorted_landlords.json", "utf8")
+  );
+  return res.send({ info: landlords });
 });
-
 
 // Not used
 // Returns an object of addresses.
 // Can be used for auto-filling search boxes for landlord grouping.
-app.get('/api/properties', (req, res) => {
-    const properties = JSON.parse(fs.readFileSync('./properties.json', 'utf8'));
-    return res.send(JSON.stringify(properties));
+app.get("/api/properties", (req, res) => {
+  const properties = JSON.parse(fs.readFileSync("./data/properties.json", "utf8"));
+  return res.send(JSON.stringify(properties));
 });
 
 // Returns an object of landlords with data on addresses, geo coords, number of properties.
 // Useful for returning a smaller number of objects
-app.get('/api/landlords-and-properties/:landlord', (req, res) => {
-    const landlordsAndProperties = JSON.parse(
-        fs.readFileSync('./landlords_and_properties.json', 'utf8')
-    );
-    return res.send(landlordsAndProperties[req.params.landlord]);
+app.get("/api/landlords-and-properties/:landlord", (req, res) => {
+  const landlordsAndProperties = JSON.parse(
+    fs.readFileSync("./data/landlords_and_properties.json", "utf8")
+  );
+  return res.send(landlordsAndProperties[req.params.landlord]);
 });
 
-
-app.get('/api/zip-code-bar-data', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('./zip_code_bar_data.json', 'utf8'));
-    return res.send(data);
+app.get("/api/zip-code-bar-data", (req, res) => {
+  const data = JSON.parse(
+    fs.readFileSync("./data/zip_code_bar_data.json", "utf8")
+  );
+  return res.send(data);
 });
 
-app.get('/api/zip-code-bar-data/:zipCode', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('./zip_code_bar_data.json', 'utf8'));
-    return res.send(data[req.params.zipCode]);
+app.get("/api/zip-code-bar-data/:zipCode", (req, res) => {
+  const data = JSON.parse(
+    fs.readFileSync("./data/zip_code_bar_data.json", "utf8")
+  );
+  return res.send(data[req.params.zipCode]);
 });
 
-app.get('/api/zip-codes/', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('./zip_codes.json', 'utf8'));
-    return res.send(data);
+app.get("/api/zip-codes/", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./data/zip_codes.json", "utf8"));
+  return res.send(data);
 });
 
-app.get('/api/cw_property_dist/', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('./city_wide_dist.json', 'utf8'));
-    return res.send(data);
+app.get("/api/cw_property_dist/", (req, res) => {
+  const data = JSON.parse(
+    fs.readFileSync("./data/city_wide_dist.json", "utf8")
+  );
+  return res.send(data);
 });
-
